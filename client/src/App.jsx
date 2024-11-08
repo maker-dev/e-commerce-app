c5cecc24a4d9836c7bc556370b065bb896d68c8f
maker-dev <anasaizen9@gmail.com>
2024-11-08 14:58:55 +0100

    COMPLETE navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,21 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Collection from "./pages/Collection"
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Product from './pages/Product';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import PlaceOrder from './pages/PlaceOrder';
-import Orders from './pages/Orders';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
 import NavBar from "./components/NavBar";
 
-
-
 function App() {
-
-  return (  
+  return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
@@ -27,10 +24,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
-        
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
